6fa5fba298772df90431ba643ddd6d9ee49527d0
zhangdaiyan <zhangdaiyan@vipkid.com.cn>
2018-12-02 13:18:52 +0800

    optimize lazyload

--- utils/lazyload/index.js
+++ utils/lazyload/index.js
@@ -2,28 +2,30 @@ export function lazyloadInit () {
   if (!window.addEventListener || !window.requestAnimationFrame) return
   window.addEventListener('scroll', calcAndLoad, false);
   window.addEventListener('resize', calcAndLoad, false);
-  const items = Array.from(document.querySelectorAll('img[data-src]'));
+  let items = Array.from(document.querySelectorAll('img[data-src]'));
   calcAndLoad();
 
   function calcAndLoad() {
-    if (!items.length) return
+    if (!items.length) {
+      window.removeEventListener('scroll', calcAndLoad, false);
+      window.removeEventListener('resize', calcAndLoad), false;
+      return
+    }
     setTimeout(function () {
-      requestAnimationFrame(function () { // throttled
-        const wt = window.pageYOffset;
-        const wb = wt + window.innerHeight;
-        items.filter(item => {
-          const rect = item.getBoundingClientRect();
-          const t = wt + rect.top;
-          const b = wt + rect.height;
-          if (b > wt && t < wb) {
-            item.src = item.dataset.src;
-            item.dataset.loaded = true;
-            return true
-          } else {
-            return false
-          }
-        })
-      });
-    }, 300);
+      const wt = window.pageYOffset;
+      const wb = wt + window.innerHeight;
+      items = items.filter(item => {
+        const rect = item.getBoundingClientRect();
+        const t = wt + rect.top;
+        const b = wt + rect.height;
+        if (b > wt && t < wb) {
+          item.src = item.dataset.src;
+          item.dataset.loaded = true;
+          return false
+        } else {
+          return true
+        }
+      })
+    }, 300/** delayed */);
   }
 }
